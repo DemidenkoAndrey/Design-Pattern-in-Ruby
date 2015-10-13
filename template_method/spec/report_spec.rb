d24619878c2d13da6fc26865fd341af58ce2b65e
Andrey Demidenko <demidenkoandy@gmail.com>
2015-10-13 14:57:14 +0300

    Remove dup and remove some code in report_spec.rb

--- template_method/spec/report_spec.rb
+++ template_method/spec/report_spec.rb
@@ -7,28 +7,24 @@ describe Report do
     expect(report.output_start).to eq(nil)
   end
 
+  it "method 'output_title' raise error" do
+    expect { report.output_title }.to raise_error(RuntimeError, "Called abastract method: output_line")
+  end
+
   it "method 'output_body_start' return nil" do
     expect(report.output_body_start).to eq(nil)
-  end
-
-  it "method 'output_body_end' return nil" do
-    expect(report.output_body_end).to eq(nil)
-  end
-
-  it "method 'output_start' return nil" do
-    expect(report.output_start).to eq(nil)
-  end
-
-  it "method 'output_end' return nil" do
-    expect(report.output_start).to eq(nil)
   end
 
   it "method 'output_line' raise error" do
     expect { report.output_line("hello") }.to raise_error(RuntimeError, "Called abastract method: output_line")
   end
 
-  it "method 'output_title' raise error" do
-    expect { report.output_title }.to raise_error(RuntimeError, "Called abastract method: output_line")
+  it "method 'output_body_end' return nil" do
+    expect(report.output_body_end).to eq(nil)
+  end
+
+  it "method 'output_end' return nil" do
+    expect(report.output_start).to eq(nil)
   end
 
   it "method 'output_report' raise error" do
