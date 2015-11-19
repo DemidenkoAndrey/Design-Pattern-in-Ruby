27e5f83268f5aa93bf36061180cf77204ee19c21
Andrey Demidenko <demidenkoandy@gmail.com>
2015-11-19 14:39:11 +0300

    Add module subject using code block

---
+++ observer/using_code_blocks/subject.rb
@@ -0,0 +1,23 @@
+module Subject
+  def initialize
+    @observers=[]
+  end
+
+  def add_observer(&observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.call(self)
+    end
+  end
+
+  def count_observers
+    @observers.count
+  end
+end
