8c80482c4b8296447dbfcf268f3c5aa506d4b829
Andrey Demidenko <demidenkoandy@gmail.com>
2015-11-19 13:16:48 +0300

    Add Employee as subject

---
+++ observer/employee.rb
@@ -0,0 +1,18 @@
+require_relative 'subject'
+class Employee
+  include Subject
+  attr_reader :name
+  attr_accessor :title, :salary
+
+  def initialize(name, title, salary)
+    super()
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+end
