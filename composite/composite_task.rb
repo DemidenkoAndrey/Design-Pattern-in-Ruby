fb947f42b53e4d79fb33166f3ba60b874f2fd107
Andrey Demidenko <demidenkoandy@gmail.com>
2015-11-23 11:01:25 +0300

    Add Composite

---
+++ composite/composite_task.rb
@@ -0,0 +1,34 @@
+class CompositeTask < Task
+  def initialize(name)
+    super(name)
+    @sub_tasks = []
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+    task.parent = self
+  end
+
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+    task.parent = nil
+  end
+
+  def get_time_required
+    @sub_tasks.map { |task| task.get_time_required }.inject(:+) || 0
+  end
+
+  def [](index)
+    @sub_tasks[index]
+  end
+
+  def []=(index, new_value)
+    @sub_tasks[index] = new_value
+  end
+
+  def total_num_of_tasks
+    @sub_tasks.map(&:total_num_of_tasks).inject(:+) || 0
+  end
+
+  alias << add_sub_task
+end
