f47e855aa0ce6c2664de3ebcbf1df33d7fe77b82
Andrey Demidenko <demidenkoandy@gmail.com>
2015-11-25 10:43:27 +0300

    Add spec for Portfolio

---
+++ iterator/Enumerable/spec/portfolio_spec.rb
@@ -0,0 +1,51 @@
+require_relative '../account'
+require_relative '../portfolio'
+
+describe Portfolio do
+  let(:account1) { Account.new("Credit", 30_000) }
+  let(:account2) { Account.new("Debit", 25_000) }
+  let(:account3) { Account.new("Credit", 15_000) }
+  let(:portfolio) { Portfolio.new }
+
+  it "has method each" do
+    expect(portfolio.respond_to?(:each)).to be true
+  end
+
+  it "has method add_account" do
+    expect(portfolio.respond_to?(:add_account)).to be true
+  end
+
+  it "method add_account add new account to array" do
+    expect { portfolio.add_account(account1) }
+      .to change { portfolio.count }.by(1)
+  end
+
+  it "iterate via each" do
+    result = ""
+    result << "Credit: 30000\n"
+    result << "Debit: 25000\n"
+    result << "Credit: 15000\n"
+    portfolio.add_account(account1)
+    portfolio.add_account(account2)
+    portfolio.add_account(account3)
+    expect do
+      portfolio.each do |account|
+        puts "#{account.name}: #{account.balance}"
+      end
+    end.to output(result).to_stdout
+  end
+
+  it "has method map" do
+    portfolio.add_account(account1)
+    portfolio.add_account(account2)
+    expect(portfolio.map(&:name)).to eq(%w(Credit Debit))
+  end
+
+  it "has method select" do
+    portfolio.add_account(account1)
+    portfolio.add_account(account2)
+    portfolio.add_account(account3)
+    expect(portfolio.select { |acc| acc.balance < 26_000 })
+      .to eq([account2, account3])
+  end
+end
