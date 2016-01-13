999956f7e77e1ae1cdfe5ec4773a8ddf8380360f
Andrey Demidenko <demidenkoandy@gmail.com>
2016-01-13 15:27:27 +0300

    Add virtual account proxy

---
+++ proxy/virtual_account_proxy.rb
@@ -0,0 +1,24 @@
+class VirtualAccountProxy
+  def initialize(&creation_block)
+    @creation_block = creation_block
+  end
+
+  def deposit(amount)
+    s = subject
+    s.deposit(amount)
+  end
+
+  def withdraw(amount)
+    s = subject
+    s.withdraw(amount)
+  end
+
+  def balance
+    s = subject
+    s.balance
+  end
+
+  def subject
+    @subject ||= @creation_block.call
+  end
+end
